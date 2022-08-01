84fbcdbb215cd3f9233af90a6fd2c73ea78cb785
Qamar Abbas <mr.qamargondal@gmail.com>
2022-08-01 22:40:23 +0500

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,41 @@
+main() {
+
+  //Data types of variables 
+  
+  //numbers k liye
+  int age=10;
+  print(age);
+
+  //decimal number k liye
+  double day= 10.5;
+  print(day);
+
+  //Jab value "" '' k ander ho
+  String month = 'December';
+  print(month);
+
+  //true or false k liye
+  bool qualified= true;
+  print(qualified);
+
+  //list k liye
+  List candidate = [1,2, 'Qamar'];
+  print(candidate);
+
+  List arr = [1,2, 'Qamar Abbas'];
+  print(arr[0]);
+  print(arr[1]);
+  print(arr[2]);
+
+  Map info = {'name': 'Qamar Abbas Gondal', 'City': 'Chiniot'};
+  print(info);
+  print(info['name']);
+  print(info['City']);
+
+  //Dynamic variable k liye ham var likhte hain aur ye data type ko khud samajh leta hai k wo konsi type hai
+  var cast = 'Gondal';
+  print(cast);
+  var phone = 03447753574;
+  print(phone);
+
+}
